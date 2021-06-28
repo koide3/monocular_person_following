bcd71e0ae3057ae7b92566bff14ba319b70efc6c
k.koide <k.koide@aist.go.jp>
2021-06-28 17:27:30 +0900

    docker for jp45

---
+++ docker/jp45/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sudo docker build --tag koide3/jetson-ros-noetic-desktop:jp45 -f Dockerfile.ros.noetic .
+sudo docker build --tag koide3/monocular_person_following:jp45 -f Dockerfile .
---
+++ docker/jp45/run.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+sudo docker run -it --rm \
+                --net host \
+                --gpus all \
+                --device /dev/video0:/dev/video0 \
+                -v $(realpath ~/.ros/camera_info):/root/.ros/camera_info \
+                koide3/monocular_person_following:jp45
