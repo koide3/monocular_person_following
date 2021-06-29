c86b34ef77bf2b0b323c6c54cedee8b9f044cf50
k.koide <k.koide@aist.go.jp>
2021-06-29 14:12:47 +0900

    dockerfile for noetic

---
+++ docker/noetic/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sudo docker build --tag koide3/jetson-ros-noetic-desktop:noetic -f Dockerfile.ros.noetic .
+sudo docker build --tag koide3/monocular_person_following:noetic -f Dockerfile .
